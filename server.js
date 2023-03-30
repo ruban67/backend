4104f6fbbc0257c9829194ad3509b6bfc17b41a6
Ruban_p <rubansmash17@gmail.com>
2023-03-31 02:12:57 +0530

    finish

--- server.js
+++ server.js
@@ -1,22 +1,50 @@
-const dotenv =require("dotenv").config();
-const express =require("express");
+const dotenv = require("dotenv").config();
+const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser =require("body-parser");
+const bodyParser = require("body-parser");
 const cors = require("cors");
+const userRoute = require("./routes/userRoute");
+const productRoute = require("./routes/productRoute");
+const contactRoute = require("./routes/contactRoute");
+const errorHandler = require("./middleWare/errorMiddleware");
+const cookieParser = require("cookie-parser");
+const path = require("path");
 
+const app = express();
 
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://pinvent-app.vercel.app"],
+    credentials: true,
+  })
+);
 
-const  app = express();
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
+// Routes Middleware
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/contactus", contactRoute);
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
+// Error Middleware
+app.use(errorHandler);
+// Connect to DB and start server
 const PORT = process.env.PORT || 5000;
-
-
-//connect to db and start server
-
-mongoose .connect(process.env.MONGO_URI)
-.then(()=>{
-    app.listen(PORT,()=>{
-        console.log(`server running on port ${PORT}`)
-    })
-})
-.catch((err)=> console.log(err))
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server Running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
